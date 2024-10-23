54806d39a5ae4e5a2333419a71502e8be11a05f4
Victor C <74551928+victorcamacaro253@users.noreply.github.com>
2024-10-22 23:46:54 -0400

    Add files via upload

---
+++ routes/productsRoutes.js
@@ -0,0 +1,9 @@
+import {Router } from 'express'
+import productController from "../controllers/productController.js";
+
+const router = Router()
+
+
+router.get('/',productController.getProducts)
+
+export default router;
